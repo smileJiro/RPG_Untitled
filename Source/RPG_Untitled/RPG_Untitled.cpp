07da1de8a04b66668ff99071212ba21495551b27
smileJiro <xodnd1478@gmail.com>
2025-12-26 15:15:45 +0900

    Init Project

---
+++ Source/RPG_Untitled/RPG_Untitled.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RPG_Untitled.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RPG_Untitled, "RPG_Untitled" );
